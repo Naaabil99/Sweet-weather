9ed6e0c0dce5636c6b9e7e29b822b6f8a9fb2ec1
Naaabil99 <nabil.irubide@gmail.com>
2021-10-22 11:51:26 +0200

    V1

---
+++ assets/js/openWeather.js
@@ -0,0 +1,105 @@
+
+var languaje = "";
+$(document).ready(function() {
+    $(".spanishlang").click(function () {
+        languaje = "es";
+    });
+
+    $(".englishlang").click(function () {
+        languaje = "en";
+    });
+});
+function OpenWeather() {
+    this.apiUrl = 'https://api.openweathermap.org/data/2.5/';
+
+
+
+
+}
+
+
+OpenWeather.prototype.getCurrentWeather = function(city) {
+
+
+    $.get(this.apiUrl + "weather?lang=" + languaje + "&q=" + city + "&appid=5440343ae5883f550724120bd6feac95", function(response) {
+        var resultcurrent="";
+        var forecast = response;
+        // Object.keys(name).length
+        if (forecast && Object.keys(forecast).length > 0) {
+            if (languaje === "es"){
+                resultcurrent += '<h2>' + "Tiempo actual" + '</h2>'
+            }
+            else{
+                resultcurrent += '<h2>' + "Actual weather" + '</h2>'
+            }
+
+            resultcurrent += '<div class=card style="width: 18rem">'
+            resultcurrent += '<img class="card-img-top" src= ' + "http://openweathermap.org/img/w/" + forecast.weather[0].icon + ".png" + ' />'
+            resultcurrent += '<div class="card-body">'
+            resultcurrent += '<h4 class="card-title">' + forecast.weather[0].main  +'</h4>'
+            resultcurrent += '<p>' + forecast.weather[0].description + '</p>' + '</div>' + '</div>'
+        } else {
+
+            resultcurrent = 'No se han encontrado issues en el repositorio de jQuery';
+        }
+
+        $('#resultcurrent').html(resultcurrent);
+    });
+};
+
+OpenWeather.prototype.get5dayWeather = function(city) {
+
+
+
+    $.get(this.apiUrl + "forecast?lang=" + languaje + "&q=" + city + "&appid=5440343ae5883f550724120bd6feac95", function (response){
+       var result5days = "";
+       var forecast = response;
+
+        if (forecast && Object.keys(forecast).length > 0) {
+
+            var days = forecast.list.filter(function (value,index){
+                return index % 8 == 0;
+            });
+
+            if (languaje === "es"){
+                result5days += '<h2>' + "Tiempo los próximos 5 días" + '</h2>'
+            }
+            else{
+                result5days += '<h2>' + "Weather in the next 5 days" + '</h2>'
+            }
+
+
+            $.each(days, function (index, day){
+                result5days += '<div class=col-sm-12>'
+                result5days += '<div class=card style="width: 18rem">'
+                result5days += '<img class="card-img-top" src= ' + "http://openweathermap.org/img/w/" + day.weather[0].icon + ".png" + ' />'
+                result5days += '<div class="card-body">'
+                result5days += '<h4 class="card-title">' + day.weather[0].main  +'</h4>'
+                result5days += '<p>' + day.dt_txt + '</p>'
+                result5days += '<p>' + day.weather[0].description + '</p>' + '</div>' + '</div>' + '</div>'
+            });
+
+
+
+
+
+        } else {
+
+            result5days = 'No se han encontrado issues en el repositorio de jQuery';
+        }
+        $('#result5days').html(result5days);
+    });
+
+}
+
+
+$(function (){
+    var openWeather = new OpenWeather();
+
+    $('#bsearch').on('click', function () {
+
+        openWeather.getCurrentWeather($('#cityName').val());
+        openWeather.get5dayWeather($('#cityName').val());
+    });
+    
+})
---
+++ assets/js/pagina.js
@@ -0,0 +1,74 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("searchbycity").style.marginLeft = "250px";
+  }
+
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("searchbycity").style.marginLeft = "0";
+  }
+
+  function setEnglish(){
+
+      $("#result5days").empty();
+      $("#resultcurrent").empty();
+      $(".header h1").text("Sweet weather yeah!")
+      $("#main h1").text("Your weather app");
+      $("#main h2").text("Know the weather all the time");
+      $("#searchbycity h1").text("Search by city:");
+      $("#searchbycity .buscar").text("City: ").append('<input type="text" id="cityName">');
+      $("#searchbycity button").text("Search");
+      $("#home").text("Home");
+      $("#buscar").text("Search").prepend('&#x1F50D;');;
+      $("#ubicacion").text("Location").prepend('&#x1f4cc;' );
+
+
+  };
+
+function setSpanish(){
+    $("#result5days").empty();
+    $("#resultcurrent").empty();
+    $(".header h1").text("¡Buen clima, sí!");
+    $("#main h1").text("Tu aplicación meteorológica");
+    $("#main h2").text("Conoce el clima todo el tiempo");
+    $("#searchbycity h1").text("Buscar por ciudad:");
+    $("#searchbycity .buscar").text("Ciudad: ").append('<input type="text" id="cityName">');
+    $("#searchbycity button").text("Buscar");
+    $("#home").text("Hogar");
+    $("#buscar").text("Buscar").prepend('&#x1F50D;');
+    $("#ubicacion").text("Ubicacion").prepend('&#x1f4cc;' );
+}
+
+  $(document).ready(function(){
+
+    var main = $("#main")
+   var buscar=  $("#searchbycity");
+
+
+   buscar.hide();
+
+    $("#buscar").click(function(){
+        $("#result5days").empty();
+        $("#resultcurrent").empty();
+      main.hide();
+      buscar.show();
+
+
+    })
+
+    $("#home").click(function(){
+
+      main.show();
+      buscar.hide();
+
+
+    });
+  });
+
+
+ // document.getElementById("mySidebar").style.width = "250px";
+  //document.getElementById("main").style.marginLeft = "250px";
+
+  
